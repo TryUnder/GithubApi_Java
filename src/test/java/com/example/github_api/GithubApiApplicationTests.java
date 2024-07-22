4dc68df26490a33e8c658cb6d62b005e21a95b4f
TryUnder <sniacyinaczej@gmail.com>
2024-07-23 00:36:04 +0200

    update tests

--- src/test/java/com/example/github_api/GithubApiApplicationTests.java
+++ src/test/java/com/example/github_api/GithubApiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.github_api;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,7 +13,8 @@ class GithubApiApplicationTests {
 	}
 
 	@Test
-	void testWorkflow() {
+	@DisplayName("Testing function: testWorkflow 2 == 2?")
+	void testForTrueWorkflow() {
 		assertEquals(2, 2);
 	}
 
