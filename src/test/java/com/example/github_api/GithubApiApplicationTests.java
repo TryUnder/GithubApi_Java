4bf6860b57573fba99b6d13aa1467cce5de5c128
TryUnder <sniacyinaczej@gmail.com>
2024-07-23 00:04:39 +0200

    modified GitHubApiApplicationTests.java to check whether workflow testing works properly

--- src/test/java/com/example/github_api/GithubApiApplicationTests.java
+++ src/test/java/com/example/github_api/GithubApiApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.github_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class GithubApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testWorkflow() {
+		assertEquals(2, 2);
 	}
 
 }
