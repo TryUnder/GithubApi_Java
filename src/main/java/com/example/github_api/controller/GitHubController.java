146d63fbf4ee6fb1461f15b5f9fdc8a43ed93478
TryUnder <sniacyinaczej@gmail.com>
2024-07-23 11:43:16 +0200

    Added implementation of GithubController.java

--- src/main/java/com/example/github_api/controller/GitHubController.java
+++ src/main/java/com/example/github_api/controller/GitHubController.java
@@ -3,6 +3,7 @@ package com.example.github_api.controller;
 import com.example.github_api.exception.UserNotFoundException;
 import com.example.github_api.model.GitHubRepository;
 import com.example.github_api.service.GitHubService;
+import com.example.github_api.exception.GlobalExceptionHandler;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -18,10 +19,29 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@RestController
+@RequestMapping("api/github")
+@RequiredArgsConstructor
 public class GitHubController {
+    private final GitHubService gitHubService;
 
+    @GetMapping("/users/{username}/repositories")
     public ResponseEntity<?> getUserRepositories(@PathVariable String username) {
-        return ResponseEntity.ok(200);
+        try {
+            List<GitHubRepository> repositories = gitHubService.getRepositories(username);
+            return ResponseEntity.ok(repositories);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new GlobalExceptionHandler());
+        }
+    }
+
+    @GetMapping("/users/{username}/repositories/{repository}/branches")
+    public ResponseEntity<?> getRepositoryBranches(@PathVariable String username, @PathVariable String repository) {
+        try {
+            List<GitHubRepository.Branch> branches = gitHubService.getBranches(username, repository);
+            return ResponseEntity.ok(branches);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new GlobalExceptionHandler());
+        }
     }
 }
