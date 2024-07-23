bcb01e318308297273105740c1c7d1103b7a6265
TryUnder <sniacyinaczej@gmail.com>
2024-07-23 11:23:04 +0200

    Added minimal implementation of GithubService.java for failing tests

--- src/main/java/com/example/github_api/service/GitHubService.java
+++ src/main/java/com/example/github_api/service/GitHubService.java
@@ -1,4 +1,22 @@
 package com.example.github_api.service;
 
+import com.example.github_api.exception.UserNotFoundException;
+import com.example.github_api.model.GitHubRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 public class GitHubService {
+
+    public List<GitHubRepository> getRepositories(String username) {
+        return List.of();
+    }
 }
