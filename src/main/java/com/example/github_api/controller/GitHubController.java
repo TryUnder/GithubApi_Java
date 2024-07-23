e34731261fb67a1c59d6bbcbd154fcb06a341212
TryUnder <sniacyinaczej@gmail.com>
2024-07-23 11:09:33 +0200

    Added minimal implementation of GitHubController.java in order to have a failing test cases

--- src/main/java/com/example/github_api/controller/GitHubController.java
+++ src/main/java/com/example/github_api/controller/GitHubController.java
@@ -1,4 +1,27 @@
 package com.example.github_api.controller;
 
+import com.example.github_api.exception.UserNotFoundException;
+import com.example.github_api.model.GitHubRepository;
+import com.example.github_api.service.GitHubService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class GitHubController {
+
+    public ResponseEntity<?> getUserRepositories(@PathVariable String username) {
+        return ResponseEntity.ok(200);
+    }
 }
